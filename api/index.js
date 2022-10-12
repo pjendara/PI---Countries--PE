b8702ea93f7aef43df309118ead8492a56620fa0
pjendara <pjendara@gmail.com>
2022-10-12 12:12:17 -0500

    conf api

--- api/index.js
+++ api/index.js
@@ -20,9 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const port = process.env.PORT || 3001;
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
