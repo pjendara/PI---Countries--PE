803870c63901b24aa9b52691b8918350e70cf4d4
pjendara <pjendara@gmail.com>
2022-10-13 12:15:55 -0500

    cambio vercel

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pi-countries-pe-production.up.railway.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://travel-app-pj.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
