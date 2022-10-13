76b19f6e453a3e94ca0562e62d8889fea22cbad9
pjendara <pjendara@gmail.com>
2022-10-13 11:33:25 -0500

    cambio puerto

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const port = process.env.PORT;
+const port = process.env.PGPORT;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
