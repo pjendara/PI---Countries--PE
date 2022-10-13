e69d195d8bb093e5ba04c4b70aca4c8bd754d8ab
pjendara <pjendara@gmail.com>
2022-10-13 11:00:38 -0500

    force true

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 const port = process.env.PORT;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
